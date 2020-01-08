933700e284eddc924355c977567edf3797314f5c
KENWAY <25846872+K-E-N-W-A-Y@users.noreply.github.com>
2020-01-08 20:32:35 +0530

    search fix attempt

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ var html = `
 </html>
 `;
 
+document.write('<script src="//cdn.jsdelivr.net/gh/K-E-N-W-A-Y/goindex/themes/material/search.js"></script>');
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request));
 });
